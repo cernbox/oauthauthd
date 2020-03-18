a4e56540279540c2722829d3ed94165ed10e1237
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2020-03-18 18:37:49 +0100

    add cache for tokens (1 million entries and 1 min TTL) and re-use mysql conn

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/cernbox/oauthauthd
+
+go 1.13
+
+require (
+	github.com/bluele/gcache v0.0.0-20190518031135-bc40bd653833
+	github.com/cernbox/gohub v1.0.3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/handlers v1.4.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.6.2 // indirect
+	go.uber.org/zap v1.14.1
+)
--- pkg/mysqluserbackend/mysqluserbackend.go
+++ pkg/mysqluserbackend/mysqluserbackend.go
@@ -5,34 +5,49 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"sync"
-	"time"
-
+	"github.com/bluele/gcache"
 	"github.com/cernbox/oauthauthd/pkg"
 	_ "github.com/go-sql-driver/mysql"
 	"go.uber.org/zap"
+	"time"
 )
 
 type Options struct {
-	Hostname string
-	Port     int
-	Username string
-	Password string
-	DB       string
-
-	Logger *zap.Logger
+	Hostname  string
+	Port      int
+	Username  string
+	Password  string
+	DB        string
+	CacheSize int
+	CacheTTL  int
+	Logger    *zap.Logger
 }
 
 func New(opt *Options) pkg.UserBackend {
+	setDefaults(opt)
+
+	cache := gcache.New(opt.CacheSize).LFU().Build()
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", opt.Username, opt.Password, opt.Hostname, opt.Port, opt.DB))
+	if err != nil {
+		panic(fmt.Sprintf("CANNOT CONNECT TO MYSQL SERVER. MAKE SURE THIS SERVICE RUNS AFTER DB IS UP AND RUNNING. HOSTNAME=%s PORT=%d DB=%s", opt.Hostname, opt.Port, opt.DB))
+	}
 
 	return &userBackend{
-		hostname: opt.Hostname,
-		port:     opt.Port,
-		username: opt.Username,
-		password: opt.Password,
-		db:       opt.DB,
+		db:       db,
 		logger:   opt.Logger,
-		cache:    &sync.Map{},
+		cache:    cache,
+		cacheTTL: time.Second * time.Duration(opt.CacheTTL),
+	}
+}
+
+func setDefaults(opt *Options) {
+	if opt.CacheSize == 0 {
+		opt.CacheSize = 1000000 // 1 million
+	}
+
+	if opt.CacheTTL == 0 {
+		opt.CacheTTL = 60 // seconds
 	}
 }
 
@@ -41,29 +56,54 @@ type userBackend struct {
 	port     int
 	username string
 	password string
-	db       string
+	db       *sql.DB
 	table    string
 
-	logger *zap.Logger
-	cache  *sync.Map
+	logger   *zap.Logger
+	cache    gcache.Cache
+	cacheTTL time.Duration
 }
 
 // TODO implement caching
 
-func (ub *userBackend) Authenticate(ctx context.Context, token string) (string, error) {
-
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", ub.username, ub.password, ub.hostname, ub.port, ub.db))
-	if err != nil {
-		ub.logger.Error("CANNOT CONNECT TO MYSQL SERVER", zap.String("HOSTNAME", ub.hostname), zap.Int("PORT", ub.port), zap.String("DB", ub.db))
-		return "", err
+// returns empty token if no entry is available.
+func (ub *userBackend) getFromCache(ctx context.Context, key string) (ti *tokenInfo) {
+	v, err := ub.cache.Get(key)
+	if err == nil {
+		if tokenInfo, ok := v.(*tokenInfo); ok {
+			ti = tokenInfo
+		}
 	}
-	defer db.Close()
+	return
+}
+
+// set key in cache with timet=-to-live in seconds
+func (ub *userBackend) storeInCache(ctx context.Context, key string, ti *tokenInfo) {
+	ub.cache.SetWithExpire(key, ti, ub.cacheTTL)
+}
+
+type tokenInfo struct {
+	userId  string
+	expires int64
+}
+
+func newTokenInfo(userId string, expires int64) *tokenInfo {
+	return &tokenInfo{userId: userId, expires: expires}
+}
+
+func (ub *userBackend) Authenticate(ctx context.Context, token string) (string, error) {
+	// check if token is still in the cache
+	cachedTokenInfo := ub.getFromCache(ctx, token)
+	if cachedTokenInfo != nil && cachedTokenInfo.userId != "" {
+		ub.logger.Info("OAUTH AUTHENTICATED BY USING CACHE", zap.String("user", cachedTokenInfo.userId))
+		return cachedTokenInfo.userId, nil
+	}
 
 	var user string
 	var expires int64
 
 	query := "SELECT user_id, expires FROM oc_oauth2_access_tokens WHERE token=?"
-	err = db.QueryRow(query, token).Scan(&user, &expires)
+	err := ub.db.QueryRow(query, token).Scan(&user, &expires)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return "", errors.New("Token not found")
@@ -78,6 +118,12 @@ func (ub *userBackend) Authenticate(ctx context.Context, token string) (string, 
 	}
 
 	ub.logger.Info("OAUTH AUTHENTICATED", zap.String("user", user))
+
+	// store in the cache
+	ti := newTokenInfo(user, expires)
+	ub.storeInCache(ctx, token, ti)
+
+	ub.logger.Info("OAUTH TOKEN STORED IN CACHE", zap.String("user", user), zap.Int64("expire", expires))
 
 	return user, nil
 }
